8e4204544bab956cbe4ec47957efe7b1bcfe2d5b
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-07-20 23:26:10 -0300

    Add files via upload

---
+++ Componentes/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './footer.css'
+
+export default function Footer() {
+    return (
+
+        <footer>&#120148;&#120160;&#120158; React</footer>
+
+    );
+}
---
+++ Componentes/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+        
+        <header>@&#120129;&#120144;&#120141;&#120131;&#120131;</header>
+    
+    );
+}
+
+export default Header;
